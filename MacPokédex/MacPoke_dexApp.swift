e9368180f0a1ca82a613c4d66bacae7d79b03d25
Jacob Ferrari <jacob.ferrari@glasshouse.qld.edu.au>
2025-04-07 11:18:27 +1000

    Initial Commit

---
+++ MacPokédex/MacPoke_dexApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacPoke_dexApp.swift
+//  MacPokédex
+//
+//  Created by Jacob on 7/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct MacPoke_dexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacPokédexTests/MacPoke_dexTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacPoke_dexTests.swift
+//  MacPokédexTests
+//
+//  Created by Jacob on 7/4/2025.
+//
+
+import Testing
+@testable import MacPoke_dex
+
+struct MacPoke_dexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
